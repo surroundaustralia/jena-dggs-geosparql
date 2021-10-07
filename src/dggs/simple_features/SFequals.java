6c424b67095e328fccd2e4ecccbd7e55b4ad6f03
david-habgood <david.habgood@surroundaustralia.com>
2021-10-07 16:32:20 +1000

    Commenting unused code

--- src/dggs/simple_features/SFequals.java
+++ src/dggs/simple_features/SFequals.java
@@ -1,23 +1,23 @@
-package dggs.simple_features;
-
-import org.apache.jena.sparql.expr.NodeValue;
-import org.apache.jena.sparql.function.FunctionBase2;
-
-
-public class SFequals extends FunctionBase2
-{
-    public NodeValue exec(NodeValue nv1, NodeValue nv2)
-    {
-        return equalsfunc(nv1, nv2) ;
-    }
-	private NodeValue equalsfunc(NodeValue nv1, NodeValue nv2) {
-		NodeValue returnval = nv2;
-		if (nv1.equals(nv2)) {
-			returnval = nv1;
-		}
-		return returnval;
-	}
-}
+//package dggs.simple_features;
+//
+//import org.apache.jena.sparql.expr.NodeValue;
+//import org.apache.jena.sparql.function.FunctionBase2;
+//
+//
+//public class SFequals extends FunctionBase2
+//{
+//    public NodeValue exec(NodeValue nv1, NodeValue nv2)
+//    {
+//        return equalsfunc(nv1, nv2) ;
+//    }
+//	private NodeValue equalsfunc(NodeValue nv1, NodeValue nv2) {
+//		NodeValue returnval = nv2;
+//		if (nv1.equals(nv2)) {
+//			returnval = nv1;
+//		}
+//		return returnval;
+//	}
+//}
 
 //public class SFequals {
 //	public Boolean equals(String s1, String s2) {
